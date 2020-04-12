b7f571363cf5c6f6ac5fee957ce811705fba7a9f
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-04-12 13:18:19 +0100

    Feat(Drift): Setup Drift Chat Bot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @include('shared.drift')
 </head>
 <body>
     <div id="app">
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -23,6 +23,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @include('shared.drift')
 </head>
 <body>
     <div id="app">
