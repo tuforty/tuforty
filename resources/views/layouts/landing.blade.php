13da2830e479c7526724355a3f0ed5e66a7bc940
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-07-04 16:21:13 +0100

    Feat(Landing): Improve Use Cases Section

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -1,46 +1,49 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Custom Meta Tags -->
-    @yield('meta-tags')
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Tuforti') }}</title>
+  <!-- Custom Meta Tags -->
+  @yield('meta-tags')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <title>{{ config('app.name', 'Tuforti') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-    <link async href="{{ asset('css/prism.css') }}" rel="stylesheet">
-    <script async src="{{ asset('js/prism.js') }}"></script>
-    @include('shared.drift')
-  </head>
-  <body>
-    <div id="app" class="landing">
-      @include('shared.navbar')
-      @yield('content')
-      @include('partials.features')
-      @include('partials.pricing')
-      @include('partials.usecases')
-      @include('shared.footer')
-    </div>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-<!-- Scripts -->
-@if (config('APP_ENV') === 'local')
-    <script src="{{ mix('js/app.js') }}"></script>
-@else
-    <script src="{{ mix('js/manifest.js') }}"></script>
-    <script src="{{ mix('js/vendor.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
-@endif
+  <!-- Styles -->
+  <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+  <link async href="{{ asset('css/prism.css') }}" rel="stylesheet">
+  <script async src="{{ asset('js/prism.js') }}"></script>
+  @include('shared.drift')
+</head>
 
-@yield('scripts')
-  </body>
+<body>
+  <div id="app" class="landing">
+    @include('shared.navbar')
+    @yield('content')
+    @include('partials.features')
+    @include('partials.usecases')
+    @include('partials.pricing')
+    @include('shared.footer')
+  </div>
+
+  <!-- Scripts -->
+  @if (config('APP_ENV') === 'local')
+  <script src="{{ mix('js/app.js') }}"></script>
+  @else
+  <script src="{{ mix('js/manifest.js') }}"></script>
+  <script src="{{ mix('js/vendor.js') }}"></script>
+  <script src="{{ mix('js/app.js') }}"></script>
+  @endif
+
+  @yield('scripts')
+</body>
+
 </html>
