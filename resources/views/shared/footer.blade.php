66e68b9b8ee3a8abcaa7ce1e9b3c7c39930b7ee6
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-07-05 18:01:35 +0100

    Feat(Nav): Include Legal Related Links

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,8 +2,9 @@
   <div class="landing__footer__content">
     <img src="{{ asset('img/logo.svg') }}" alt="Logo" width="50" height="50">
     <ul>
-      <li>Terms of Use</li>
-      <li><a href="https://docs.tuforty.com/">Documentation</a></li>
+      <li><a href="https://legals.tuforty.com">Terms of use</a></li>
+      <li><a href="https://legals.tuforty.com/privacy">Privacy Policy</a></li>
+      <li><a href="https://docs.tuforty.com">Documentation</a></li>
     </ul>
     <p class="copy">&copy; Tuforti</span> {{ date("Y") }}</p>
   </div>
