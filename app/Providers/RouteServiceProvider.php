98073d75025ea4bc806b5fb99a2b0b3871be45a7
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-07-06 05:05:23 +0100

    Fix(Auth): Switch Middleware Order

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -66,7 +66,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api/v1')
-            ->middleware(['verified', 'api'])
+            ->middleware(['api', 'verified'])
             ->namespace($this->namespace)
             ->group(base_path('routes/api.v1.php'));
     }
